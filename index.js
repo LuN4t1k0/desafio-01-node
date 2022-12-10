b7d16e696954dcf2512b435177915bf48c2cdad9
Cristian Venegas <cristian.venegas@materoceano.com>
2022-12-10 20:22:13 -0300

    Desafio completado

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const fs = require("fs")
-const {registrarCita, leerCitas} = require("./operaciones")
+const fs = require("fs");
+const { registrarCita, leerCitas } = require("./operaciones");
+
+const arg = process.argv.slice(2);
+
+const seleccionarOperacion = (alternativa) => {
+  if (alternativa === "registrar") {
+    registrarCita(arg[1], arg[2], arg[3], arg[4], arg[5]);
+  } else if (alternativa === "leer") {
+    leerCitas();
+  } else {
+    console.log("Debes Registrar o leer una cita");
+  }
+};
+
+seleccionarOperacion(arg[0]);
--- operaciones.js
+++ operaciones.js
@@ -1,12 +1,29 @@
-const registrarCita = () => {
+const fs = require("fs");
+const { uid } = require("uid");
 
-}
+const registrarCita = ( name, age, type, color, illness ) => {
+  let fecha = new Date();
+  const archivo = fs.readFileSync("citas.json", "utf8");
+  const dataExistente = JSON.parse(archivo);
+
+  const nuevaCita = {
+    id: uid(),
+    name,
+    age,
+    type,
+    color,
+    illness,
+    reservedAT: `${fecha.toLocaleDateString()}, ${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}`,
+  };
+  fs.writeFileSync("citas.json", JSON.stringify([...dataExistente, nuevaCita]));
+};
 
 const leerCitas = () => {
-
-}
+  const archivo = fs.readFileSync("citas.json", "utf8");
+  JSON.parse(archivo).forEach((x) => console.log(x));
+};
 
 module.exports = {
   registrarCita,
-  leerCitas
-}
+  leerCitas,
+};
